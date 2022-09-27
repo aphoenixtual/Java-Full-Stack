98f3cabf3bb7539708a21fda0b7352fd667e6677
Aayush Patel <ayushp264@gmail.com>
2022-09-27 14:11:35 +0530

    Update DataSourceConfiguration.java

--- 1-Spring/2-Spring-core-projects/2-Transfer-service-spring-web/src/main/java/com/example/config/DataSourceConfiguration.java
+++ 1-Spring/2-Spring-core-projects/2-Transfer-service-spring-web/src/main/java/com/example/config/DataSourceConfiguration.java
@@ -17,7 +17,7 @@ public class DataSourceConfiguration {
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/bankdb");
         dataSource.setUsername( "root" );
-        dataSource.setPassword( "Namonamo@311" );
+        dataSource.setPassword( "password" );
         return dataSource;
     }
 
